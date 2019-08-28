5ef3c254253a3f2deac82c13a7c5defeaeb150c7
Anwesh Budhathoki <abperni@gmail.com>
2019-08-28 22:59:05 +0545

    First Commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const http = require('http');
+const express = require('express');
+const app = express();
+const server = http.createServer(app);
+const socketIO = require('socket.io');
+const io = socketIO(server);
+const publicPath = path.join(__dirname, './public');
+const port = process.env.PORT || 3000;
+
+app.use(express.static(publicPath));
+
+io.on('connection', (socket) => {
+  console.log('New user connected');
+
+  socket.on('disconnect', () => {
+    console.log('User was disconnected');
+  });
+});
+
+server.listen(port, () => {
+  console.log(`Server is up on ${port}`);
+});
